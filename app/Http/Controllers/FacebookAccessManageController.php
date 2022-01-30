4184c9824092876ed2101ec8944e1a22bb40134c
Md. Abdul Awal <abdulawalbd121@gmail.com>
2022-01-30 11:28:43 +0600

    update

--- app/Http/Controllers/FacebookAccessManageController.php
+++ app/Http/Controllers/FacebookAccessManageController.php
@@ -22,70 +22,69 @@ class FacebookAccessManageController extends Controller
     }
 
     public function getFacebookLoginUrl(Request $request) {
-        
-        $permissions = ['email']; // Optional permissions
-        $loginUrl =$this->fb_helper->getLoginUrl(route('getFacebookCallbackUrl'), $permissions);
-        return redirect($loginUrl);
-	}
+      $permissions = ['email','pages_read_user_content','pages_show_list']; // Optional permissions
+      $loginUrl =$this->fb_helper->getLoginUrl(route('getFacebookCallbackUrl'), $permissions);
+      return redirect($loginUrl);
+    }
 
     public function getFacebookCallbackUrl(Request $request) {
-        if (isset($request->state)) {
-            $this->fb_helper->getPersistentDataHandler()->set('state', $request->state);
+      if (isset($request->state)) {
+          $this->fb_helper->getPersistentDataHandler()->set('state', $request->state);
+      }
+
+      try {
+          $accessToken = $this->fb_helper->getAccessToken();
+        } catch(\Facebook\Exceptions\FacebookResponseException $e) {
+          // When Graph returns an error
+          echo 'Graph returned an error: ' . $e->getMessage();
+          exit;
+        } catch(\Facebook\Exceptions\FacebookSDKException $e) {
+          // When validation fails or other local issues
+          echo 'Facebook SDK returned an error: ' . $e->getMessage();
+          exit;
         }
-
-        try {
-            $accessToken = $this->fb_helper->getAccessToken();
-          } catch(\Facebook\Exceptions\FacebookResponseException $e) {
-            // When Graph returns an error
-            echo 'Graph returned an error: ' . $e->getMessage();
-            exit;
-          } catch(\Facebook\Exceptions\FacebookSDKException $e) {
-            // When validation fails or other local issues
-            echo 'Facebook SDK returned an error: ' . $e->getMessage();
-            exit;
-          }
-           
-          if (!isset($accessToken)) {
-            if ($this->fb_helper->getError()) {
-              header('HTTP/1.0 401 Unauthorized');
-              echo "Error: " . $this->fb_helper->getError() . "\n";
-              echo "Error Code: " . $this->fb_helper->getErrorCode() . "\n";
-              echo "Error Reason: " . $this->fb_helper->getErrorReason() . "\n";
-              echo "Error Description: " . $this->fb_helper->getErrorDescription() . "\n";
-            } else {
-              header('HTTP/1.0 400 Bad Request');
-              echo 'Bad request';
-            }
-            exit;
-          }
-           
-          if(!$accessToken->isLongLived()){
-            // Exchanges a short-lived access token for a long-lived one
-            try {
-              $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
-            } catch (\Facebook\Exceptions\FacebookSDKException $e) {
-              echo "<p>Error getting long-lived access token: " . $e->getMessage() . "</p>\n\n";
-              exit;
-            }
-          }
-           
-          //$fb->setDefaultAccessToken($accessToken);
-           
-          # These will fall back to the default access token
-        //   $res    =   $this->fb->get('/me',$accessToken->getValue());
-        //   $fbUser =   $res->getDecodedBody();
-           
-           
-        //   $resImg     =   $this->fb->get('/me/picture?type=large&redirect=false',$accessToken->getValue());
-        //   $picture    =   $resImg->getGraphObject();
-           
-           
-        //   $_SESSION['fbUserId']       =   $fbUser['id'];
-        //   $_SESSION['fbUserName']     =   $fbUser['name'];
-        //   $_SESSION['fbAccessToken']  =   $accessToken->getValue();
           
-        DB::table('admin_config')->where('name', 'facebook_page_access_token')->update(['value' => $accessToken->getValue()]);
+        if (!isset($accessToken)) {
+          if ($this->fb_helper->getError()) {
+            header('HTTP/1.0 401 Unauthorized');
+            echo "Error: " . $this->fb_helper->getError() . "\n";
+            echo "Error Code: " . $this->fb_helper->getErrorCode() . "\n";
+            echo "Error Reason: " . $this->fb_helper->getErrorReason() . "\n";
+            echo "Error Description: " . $this->fb_helper->getErrorDescription() . "\n";
+          } else {
+            header('HTTP/1.0 400 Bad Request');
+            echo 'Bad request';
+          }
+          exit;
+        }
+          
+        if(!$accessToken->isLongLived()){
+          // Exchanges a short-lived access token for a long-lived one
+          try {
+            $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+          } catch (\Facebook\Exceptions\FacebookSDKException $e) {
+            echo "<p>Error getting long-lived access token: " . $e->getMessage() . "</p>\n\n";
+            exit;
+          }
+        }
+          
+        //$fb->setDefaultAccessToken($accessToken);
+          
+        # These will fall back to the default access token
+      //   $res    =   $this->fb->get('/me',$accessToken->getValue());
+      //   $fbUser =   $res->getDecodedBody();
+          
+          
+      //   $resImg     =   $this->fb->get('/me/picture?type=large&redirect=false',$accessToken->getValue());
+      //   $picture    =   $resImg->getGraphObject();
+          
+          
+      //   $_SESSION['fbUserId']       =   $fbUser['id'];
+      //   $_SESSION['fbUserName']     =   $fbUser['name'];
+      //   $_SESSION['fbAccessToken']  =   $accessToken->getValue();
+        
+      DB::table('admin_config')->where('name', 'facebook_page_access_token')->update(['value' => $accessToken->getValue()]);
 
-        return redirect('/');
-    }
+      return redirect('/');
+  }
 }
