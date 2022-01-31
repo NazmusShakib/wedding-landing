57ba0a30d0e974e67a202933eee844ccaa5f6a02
Md. Abdul Awal <abdulawalbd121@gmail.com>
2022-01-31 17:19:58 +0600

    update

--- resources/views/layouts/footerScripts.blade.php
+++ resources/views/layouts/footerScripts.blade.php
@@ -43,31 +43,6 @@
   }(document, 'script', 'facebook-jssdk'));
 </script>
 
-
-{{-- <script>
-  var chatbox = document.getElementById('fb-customer-chat');
-  chatbox.setAttribute("page_id", "{{ config('facebook_page_id') }}");
-  chatbox.setAttribute("attribution", "biz_inbox");
-</script> --}}
-
-<!-- Your SDK code -->
-{{-- <script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      xfbml            : true,
-      version          : 'v12.0'
-    });
-  };
-
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-</script> --}}
-
 <script src="https://apps.elfsight.com/p/platform.js" defer></script>
 <div class="elfsight-app-98528b37-adf5-4939-b459-673240c79542"></div>
 @stack('js')
--- resources/views/pages/contactUs.blade.php
+++ resources/views/pages/contactUs.blade.php
@@ -18,8 +18,8 @@
     <div class="contact-section pt-0 pb-60">
         <div class="container-fluid">
             <div class="row pb-60">
-                <div class="col-lg-7 col-md-12 mb-30">
-                    <div style="height: 400px;" id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="col-lg-12 mb-30">
+                    <div style="height: 700px;" id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                         <ol class="carousel-indicators">
                             <li data-target="#carouselExampleControls" data-slide-to="0" class="active"></li>
                             <li data-target="#carouselExampleControls" data-slide-to="1"></li>
@@ -27,74 +27,72 @@
                           </ol>
                         <div class="carousel-inner" style="height: 100%;">
                           <div class="carousel-item active" style="height: 100%;">
-                            <img style="min-height: 400px;" class="d-block w-100" src="images/contact-us-1.jpg" alt="First slide">
-                            <div class="carousel-caption d-none d-md-block">
+                            <img style="min-height: 700px;" class="d-block w-100" src="images/contact-us-1.jpg" alt="First slide">
+                            {{-- <div class="carousel-caption d-none d-md-block">
                                 <div style="display: inline-block;padding: 20px;padding-bottom: 0;border-radius: 5px;background-color: rgba(0,0,0,0.5); margin-bottom:20px;">
                                     <h5 class="text-white"><i class="ti-location-pin"></i> <b class="text-white">Bangladesh</b></h5>
                                     <p class="text-white">{!! config('office_address') !!}</p>
                                 </div>
-                            </div>
+                            </div> --}}
                           </div>
                           <div class="carousel-item">
-                            <img style="min-height: 400px;" class="d-block w-100" src="images/topbanner-1.jpg" alt="Second slide">
-                            <div class="carousel-caption d-none d-md-block" >
+                            <img style="min-height: 700px;" class="d-block w-100" src="images/topbanner-1.jpg" alt="Second slide">
+                            {{-- <div class="carousel-caption d-none d-md-block" >
                                 <div style="display: inline-block;padding: 20px;padding-bottom: 0;border-radius: 5px;background-color: rgba(0,0,0,0.5);margin-bottom:20px;">
                                     <h5 class="text-white"><i class="ti-location-pin"></i> <b class="text-white">Bangladesh</b></h5>
                                     <p class="text-white">{!! config('office_address') !!}</p>
                                 </div>
-                            </div>
+                            </div> --}}
                           </div>
                           <div class="carousel-item">
-                            <img style="min-height: 400px;" class="d-block w-100" src="images/topbanner-1.jpg" alt="Third slide">
-                            <div class="carousel-caption d-none d-md-block">
+                            <img style="min-height: 700px;" class="d-block w-100" src="images/topbanner-1.jpg" alt="Third slide">
+                            {{-- <div class="carousel-caption d-none d-md-block">
                                 <div style="display: inline-block;padding: 20px;padding-bottom: 0;border-radius: 5px;background-color: rgba(0,0,0,0.5);margin-bottom:20px;">
                                     <h5 class="text-white"><i class="ti-location-pin"></i> <b class="text-white">Bangladesh</b></h5>
                                     <p class="text-white">{!! config('office_address') !!}</p>
                                 </div>
-                            </div>
+                            </div> --}}
                           </div>
                         </div>
+
+                        <div class="col-lg-4 contact_form" style="position: absolute;top: 0;right: 0;background: rgba(0,123,255,0.5);height: 100%;padding-top:15px;padding-bottom:15px;">
+                            <div class="form-group">
+                                <p class="text-white"><i class="ti-location-pin"></i> <b class="text-white">Bangladesh</b></p>
+                                <p class="mb-0 text-white">{!! config('office_address') !!}</p>
+                                <br>
+                            </div>
+                            <h5 class="text-white">We would like to hear from you</h5>
+                            <form method="GET" action="#" class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <input name="name" type="text" class="form-control" placeholder="Name" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <input name="email" type="text" class="form-control" placeholder="Email">
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <input name="subject" type="text" class="form-control" placeholder="Subject">
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <textarea name="message" id="message" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <input type="submit" class="btn btn-contact" value="Say Hello!">
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+
                       </div>
-                    {{-- <div class="line p-30">
-                        <p><i class="ti-location-pin"></i> <b>Bangladesh</b></p>
-                        <p class="mb-0">{!! config('office_address') !!}</p>
-                        <br>
-                        <br>
-                        <div class="separator"></div>
-                        <p>{{ config('contact_number') }}
-                        <br>{{ config('contact_email') }}</p>
-                    </div> --}}
-                </div>
-                <div class="col-lg-5 col-md-12 mb-30">
-                    {{-- <h3 class="pwe-about-heading">Contact Form</h3> --}}
-                    <p>We would like to hear from you</p>
-                    <form method="GET" action="#" class="row">
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <input name="name" type="text" class="form-control" placeholder="Name" required>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <input name="email" type="text" class="form-control" placeholder="Email">
-                            </div>
-                        </div>
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                <input name="subject" type="text" class="form-control" placeholder="Subject">
-                            </div>
-                        </div>
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                <textarea name="message" id="message" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
-                            </div>
-                        </div>
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                <input type="submit" class="btn btn-contact" value="Say Hello!">
-                            </div>
-                        </div>
-                    </form>
+                    
                 </div>
                 
             </div>
