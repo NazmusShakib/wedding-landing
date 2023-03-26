41ae96084385a0fe05c1e6e2d114d237f7bff554
Shuvo <nshakib.se@gmail.com>
2023-03-26 15:38:09 +0100

    Map location and whatsapp number changed

--- resources/views/pages/section/google_map.blade.php
+++ resources/views/pages/section/google_map.blade.php
@@ -2,8 +2,26 @@
 <div class="map-section">
     <div class="row">
         <div class="col-md-12 animate-box" data-animate-effect="fadeInLeft">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7302.167266414496!2d90.393164!3d23.780036!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x485f1db6ac1ab68!2sShahjahan%20Wedding%20Planner%20%26%20Event%20Management%20Ltd.!5e0!3m2!1sen!2sus!4v1642699919303!5m2!1sen!2sus" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-            {{-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1818.9123250374973!2d89.03483124313816!3d24.24791097955714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fc19480ab9e8f1%3A0xb81c6954c89f1ccb!2sWalia%20Bazar!5e0!3m2!1sen!2sbd!4v1641884470509!5m2!1sen!2sbd" style="border:0;height: 100% !important;width: 100% !important;" allowfullscreen="" loading="lazy"></iframe> --}}
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7301.985000416858!2d90.3918784!3d23.7832814!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c7ffe09b983b%3A0xe8795910f7b3c97d!2sSketch%20Imagination!5e0!3m2!1sen!2suk!4v1679841147685!5m2!1sen!2suk"
+                width="100%"
+                height="450"
+                style="border: 0"
+                allowfullscreen=""
+                loading="lazy"
+            ></iframe>
+            {{--
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1818.9123250374973!2d89.03483124313816!3d24.24791097955714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fc19480ab9e8f1%3A0xb81c6954c89f1ccb!2sWalia%20Bazar!5e0!3m2!1sen!2sbd!4v1641884470509!5m2!1sen!2sbd"
+                style="
+                    border: 0;
+                    height: 100% !important;
+                    width: 100% !important;
+                "
+                allowfullscreen=""
+                loading="lazy"
+            ></iframe>
+            --}}
         </div>
     </div>
 </div>
