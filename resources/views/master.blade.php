212103785e0c56e741b6bec51bda0525f18bbbe8
Shuvo <nshakib.se@gmail.com>
2023-05-02 23:36:54 +0100

    Masonry portfolio

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,45 +1,47 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html lang="en">
-<head>
-    @include('layouts.head')
-    <style>
-        .mobile-top-logo {
-            display: none;
-        }
-        @media(max-width:767px){
+    <head>
+        @include('layouts.head')
+        <style>
             .mobile-top-logo {
-                position: absolute;
-                z-index: 9999;
-                display: inherit;
-                width: 100%;
-                height: 140px;
-                text-align: center;
-                z-index: 999;
+                display: none;
             }
-            .mobile-top-logo img{
-                padding-top: 10px;
-                max-width: 120px;
+            @media (max-width: 767px) {
+                .mobile-top-logo {
+                    position: absolute;
+                    z-index: 9999;
+                    display: inherit;
+                    width: 100%;
+                    height: 140px;
+                    text-align: center;
+                    z-index: 999;
+                }
+                .mobile-top-logo img {
+                    padding-top: 10px;
+                    max-width: 120px;
+                }
             }
-        }
-    </style>
-</head>
-<body>
-    <div id="pwe-page"> 
-        <a href="#" class="js-pwe-nav-toggle pwe-nav-toggle"><i></i></a>
-        <div class="mobile-top-logo">
-            <img src="{{ asset('images/sketch-logo-black.png') }}" alt="Logo">
-        </div>
-        
-        {{-- <div id="myid" class="img"></div> --}}
-        
-        @include('layouts.sidebar')
+        </style>
+    </head>
+    <body>
+        <div id="pwe-page">
+            <a href="#" class="js-pwe-nav-toggle pwe-nav-toggle"><i></i></a>
+            <div class="mobile-top-logo">
+                <img
+                    src="{{ asset('images/sketch-logo-black.png') }}"
+                    alt="Logo"
+                />
+            </div>
 
-        <div id="pwe-main">
-            @yield('body')
-            
-            @include('layouts.footer')
+            {{--
+            <div id="myid" class="img"></div>
+            --}} @include('layouts.sidebar')
+
+            <div id="pwe-main">
+                @yield('body') @stack('custom-style')
+                @include('layouts.footer')
+            </div>
+            @include('layouts.footerScripts')
         </div>
-        @include('layouts.footerScripts')
-    </div>
-</body>
+    </body>
 </html>
--- resources/views/pages/portfolioDetails.blade.php
+++ resources/views/pages/portfolioDetails.blade.php
@@ -35,36 +35,97 @@
                 </p>
             </div>
         </div>
+
         <!-- Gallery -->
         <div class="row mb-60">
+			<div id="gallery">
+				@if($portfolio['videos'])
+					@foreach ($portfolio['videos'] as $video)
+						<div class="gallery-item animate-box" data-animate-effect="fadeInLeft">
+							<a href="{{ $video }}" title="{{ $portfolio['name'] }}" class="magnific-youtube">
+								<div class="gallery-box">
+									<div class="gallery-img"> <img src="{{ asset('images/video_thumbnail.jpg') }}" class="img-fluid mx-auto d-block" alt="work-img"> </div>
+									<div class="gallery-detail text-center"> <i class="ti-plus"></i> </div>
+								</div>
+							</a>
+						</div>
+					@endforeach
+				@endif
 
-            @if($portfolio['videos'])
-            @foreach ($portfolio['videos'] as $video)
-                <div class="col-md-4 gallery-item animate-box" data-animate-effect="fadeInLeft">
-                    <a href="{{ $video }}" title="{{ $portfolio['name'] }}" class="magnific-youtube">
-                        <div class="gallery-box">
-                            <div class="gallery-img"> <img src="{{ asset('images/video_thumbnail.jpg') }}" class="img-fluid mx-auto d-block" alt="work-img"> </div>
-                            <div class="gallery-detail text-center"> <i class="ti-plus"></i> </div>
-                        </div>
-                    </a>
-                </div>
-            @endforeach
-            @endif
-
-            @if($portfolio['pictures'])
-            @foreach ($portfolio['pictures'] as $picture)
-                <div class="col-md-4 gallery-item animate-box" data-animate-effect="fadeInLeft">
-                    <a href="{{ Storage::url($picture) }}" title="{{ $portfolio['name'] }}" class="img-zoom">
-                        <div class="gallery-box">
-                            <div class="gallery-img"> <img src="{{ Storage::url($picture) }}" class="img-fluid mx-auto d-block" alt="work-img"> </div>
-                            <div class="gallery-detail text-center"> <i class="ti-plus"></i> </div>
-                        </div>
-                    </a>
-                </div>
-            @endforeach
-            @endif
+				@if($portfolio['pictures'])
+					@foreach ($portfolio['pictures'] as $picture)
+						<div class="gallery-item animate-box" data-animate-effect="fadeInLeft">
+							<a href="{{ Storage::url($picture) }}" title="{{ $portfolio['name'] }}" class="img-zoom">
+								<div class="gallery-box">
+									<div class="gallery-img"> <img src="{{ Storage::url($picture) }}" class="img-fluid mx-auto d-block" alt="work-img"> </div>
+									<div class="gallery-detail text-center"> <i class="ti-plus"></i> </div>
+								</div>
+							</a>
+						</div>
+					@endforeach
+				@endif
+			</div>
         </div>
+
     </div>
 </div>
 <x-client-component/>
 @stop
+
+@push('custom-style')
+<style>
+    .gallery-item {
+        position: relative;
+        padding-top: 0px;
+    }
+    #gallery {
+        line-height: 0;
+        -webkit-column-count: 4; /* split it into 5 columns */
+        -webkit-column-gap: 5px; /* give it a 5px gap between columns */
+        -moz-column-count: 4;
+        -moz-column-gap: 5px;
+        column-count: 4;
+        column-gap: 5px;
+    }
+    #gallery img {
+        width: 100% !important;
+        height: auto !important;
+        margin-bottom: 5px; /* to match column gap */
+    }
+    @media (max-width: 1600px) {
+        #gallery {
+            -moz-column-count: 3;
+            -webkit-column-count: 3;
+            column-count: 3;
+        }
+    }
+    @media (max-width: 1200px) {
+        #gallery {
+            -moz-column-count: 3;
+            -webkit-column-count: 3;
+            column-count: 3;
+        }
+    }
+    @media (max-width: 1000px) {
+        #gallery {
+            -moz-column-count: 3;
+            -webkit-column-count: 3;
+            column-count: 3;
+        }
+    }
+    @media (max-width: 800px) {
+        #gallery {
+            -moz-column-count: 2;
+            -webkit-column-count: 2;
+            column-count: 2;
+        }
+    }
+    @media (max-width: 400px) {
+        #gallery {
+            -moz-column-count: 1;
+            -webkit-column-count: 1;
+            column-count: 1;
+        }
+    }
+</style>
+@endpush
